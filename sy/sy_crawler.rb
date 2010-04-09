abe90f002346671cc390729bfca30d68ccbeccff
Eduardo Lanchares <edulanchi@gmail.com>
2010-04-09 02:29:34 +0200

    added pstore storage and fixed some bugs

--- sy/sy_crawler.rb
+++ sy/sy_crawler.rb
@@ -4,7 +4,7 @@ require 'anemone'
 require 'nokogiri'
 require 'harmony'
 
-class VisualizationParser
+class Visualization
 
   attr_accessor :language
   attr_accessor :subtitles
@@ -15,15 +15,18 @@ class VisualizationParser
     @language = node[1].content
     @subtitles = node[2].content
 
-    crypted_online_link = node[0].xpath('.//a')[0].attribute('href').value
+    crypted_online_link = node[0].xpath('.//a')[0].attribute('href').content
     # Request online link
     doc = Nokogiri::HTML(Harmony::Page.fetch(crypted_online_link).to_html)
     @online_link = doc.xpath('//span/b/a')[0].attribute('href').content
 
-    crypted_download_link = doc.xpath('//h3/a')[1].attribute('href').content unless doc.xpath('//h3/a').empty?
-    # Request donwload link
-    doc = Nokogiri::HTML(Harmony::Page.fetch(crypted_download_link).to_html)
-    @download_link = doc.xpath('//h3/a')[0].attribute('href').content
+    download_node = doc.xpath('//h3/a')[1]
+    if download_node
+      crypted_download_link = download_node.attribute('href').content
+      # Request donwload link
+      doc = Nokogiri::HTML(Harmony::Page.fetch(crypted_download_link).to_html)
+      @download_link = doc.xpath('//h3/a')[0].attribute('href').content
+    end
   end
 
   def to_yml
@@ -37,19 +40,21 @@ class VisualizationParser
 
 end
 
-class EpisodeParser
+class Episode
 
   attr_accessor :title
+  attr_accessor :serie
   attr_accessor :visualizations
 
-  def initialize(doc)
+  def initialize doc
     @title = doc.xpath('//h1')[-1].content
+    @serie = doc.xpath('//h2/a')[0].content
     @visualizations = []
 
-    table_node = doc.xpath('//table').first
+    table_node = doc.xpath('//table')[0]
     if table_node
       @visualizations = table_node.xpath('tr')[1..-1].collect do |tr|
-        VisualizationParser.new(tr.xpath('td/div/span')) rescue nil
+        Visualization.new(tr.xpath('td/div/span')) rescue nil
       end.compact
     end
   end
@@ -63,6 +68,25 @@ class EpisodeParser
 
 end
 
+class Serie
+
+  attr_accessor :name
+  attr_accessor :episodes
+
+  def initialize doc
+    @name = doc.xpath('//h1/a')[1].content
+    @episodes = []
+  end
+
+  def to_yml
+    {
+      :name => @name,
+      :episodes => @episodes.collect { |e| e.to_yml }
+    }
+  end
+
+end
+
 # Start url
 URL = "http://www.seriesyonkis.com"
 # User agent for the crawler
@@ -70,13 +94,16 @@ UA = "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; es-ES)"
 # Patterns
 SERIE_PATTERN = "#{URL}\/serie\/(.*)\/"
 EPISODE_PATTERN = "#{URL}\/capitulo\/.*\/(.*)\/\\d+\/"
+# File constants
+STORE_FILE = "links.pstore"
+DUMP_FILE = "series.yml"
 
-# Starts goear crawling process
-Anemone.crawl(URL, :user_agent => UA) do |anemone|
+# Starts crawling process
+Anemone.crawl(URL, :storage => Anemone::Storage.PStore(STORE_FILE),
+                   :user_agent => UA,
+                   :verbose => true) do |anemone|
 
-  #TODO: Group episodes by series
-
-  # Episodes list
+  series = []
   episodes = []
 
   # Focus crawler to follow series or episodes urls
@@ -84,14 +111,26 @@ Anemone.crawl(URL, :user_agent => UA) do |anemone|
     page.links.select { |link| link.to_s =~ /#{SERIE_PATTERN}|#{EPISODE_PATTERN}/ }
   end
 
+  anemone.on_pages_like(/#{SERIE_PATTERN}/) do |page|
+    series << Serie.new(page.doc) unless page.error
+  end
+
   # Parses episodes urls
   anemone.on_pages_like(/#{EPISODE_PATTERN}/) do |page|
-    episodes << EpisodeParser.new(page.doc)
+    episodes << Episode.new(page.doc) unless page.error
   end
 
   # Dump information
   anemone.after_crawl do |page|
-    File.open("data.yml", "w") { |f| YAML.dump(episodes.collect { |e| e.to_yml }, f) }
+    File.open(DUMP_FILE, 'w') do |f|
+      obj = series.collect do |s|
+        episodes.inject(s.episodes) do |acum,e|
+          acum << e if s.name.downcase == e.serie.downcase
+        end
+      end.to_yml
+
+      YAML.dump(obj, f)
+    end
   end
 
 end
